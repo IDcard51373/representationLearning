add funtion to make images BUT !!!! find a big bug that Y will became 0 after normalization. So, I have to cut them into small parts. ****!

src/kits.py
@@ -4,9 +4,12 @@ import os
 from PIL import Image
 import numpy as np
 import random
+import copy
 
 max_iter_times = 40
 
+img_h = 192
+img_w = 168
 
 """
 打开目录下的图片
@@ -32,6 +35,7 @@ omp算法
 
 
 def omp(D, y, t):
+
     n_d = D.shape[1]  # n_d:D的列数，即条目数量
     n_y = y.shape[1]  # n_y：y的个数
     l_dy = D.shape[0]  # l_dy:D的行数，理论上D和y的行数相同
@@ -52,8 +56,14 @@ def omp(D, y, t):
             x_k = np.dot(np.linalg.pinv(a), yi)  # 重新计算表达
             r = yi - np.dot(a, x_k)  # 得到新的残差
         temp = np.zeros((n_d, 1))
-        temp[index,0] = x_k
-        x[:, i] = np.array(temp).reshape(n_d)  # 格式要转换一下才能赋值进去
+        # temp[index, 0] = x_k
+
+        # x[:, i] = np.array(temp).reshape(n_d)
+        # 格式要转换一下才能赋值进去
+        temp[index] = x_k.reshape((t, 1))
+        temp = np.array(temp).reshape(n_d)
+        x[:, i] = temp
+        # print(temp)
     return x
 
 
@@ -82,8 +92,10 @@ def k_svd(Y, S, E, K):
         Y[:, i] = (Y[:, i] - mean) / norm
     # 迭代
     for j in range(max_iter_times):
-        x = omp(D, Y, S)
+        X = omp(D, Y, S)
+        print(X)
         e = np.linalg.norm(Y - np.dot(D, X))
+        print("迭代次数" + str(j)+"误差："+str(e))
         if e < E:
             break
         # 逐行调整
@@ -107,11 +119,30 @@ def k_svd(Y, S, E, K):
 输入 imgs(一组原始测试数据，shape=m*n)，p(像素缺失率)
 输出 imgs_lossed（shape=m*K,像素缺失的测试数据）
 """
-def loss_pixel(imgs,p = 50):
-    num = int(p*0.01*imgs.shape[0])
-    loss = np.random.randint(0, high = imgs.shape[0]-1,size = num)
+
+
+def loss_pixel(p_imgs, p):
+    imgs = copy.deepcopy(p_imgs)
+    num = int(p * 0.01 * imgs.shape[0])
+    loss = np.random.randint(0, high=imgs.shape[0] - 1, size=num)
     for i in range(imgs.shape[1]):
         for j in range(num):
-            imgs[loss[j],i] = 0
+            imgs[loss[j], i] = 0
     return imgs
 
+
+"""
+生成图像
+输入：灰度矩阵
+输出：图像img
+"""
+
+
+def to_img(p_data):
+    data = copy.deepcopy(p_data)
+    if len(data.shape) == 1:
+        data = data.reshape(img_h, img_w)
+    elif data.shape[1] == 1:
+        data = data.reshape(img_h, img_w)
+    img = Image.fromarray(data)
+    return img

src/test.py
@@ -6,15 +6,18 @@ import numpy as np
 import random
 import kits
 
+#一些训练参数
 train_num=30
 max_error=1e-5
 row_x=30
-k_d=60
+k_d=300
+loss_p = 50
 # 打开数据集
-data = np.array(kits.openImages("resources")).T
+data = np.array(kits.openImages("src\\resources")).T
 
 # 1. 将数据集划分为训练集和测试集；这里前三十个作为训练集 后九个作为测试机
-Y_train=data[:,0:train_num-1]
+Y_train=data[:,:train_num]
+
 Y_test=data[:,train_num:]
 
 
@@ -22,17 +25,20 @@ Y_test=data[:,train_num:]
 D=kits.k_svd(Y_train,row_x,max_error,k_d)
 
 # 3. 对测试集中的人脸图像进行像素缺失处理；
-Y_lossed=kits.loss_pixel(Y_test)
-
+Y_lossed=kits.loss_pixel(Y_test,loss_p)
 
 # 4. 在测试集上利用字典得到稀疏表达，然后使用字典重建人脸图像；
 representation=kits.omp(D,Y_lossed,row_x)
-
-# 5. 计算重建人脸图像与原始图像的平均误差。
 Y_represented=np.dot(D,representation)
+
+# 5. 计算重建人脸图像与原始图像的平均误差。并输出图像
 r=Y_test-Y_represented
 e=[]
 for i in range(Y_test.shape[1]):
     e.append(np.linalg.norm(r[i]))
+    kits.to_img(Y_test[:,i]).save("C:\\out\\"+str(i)+"_test.pgm")
+    kits.to_img(Y_lossed[:,i]).save("C:\\out\\" + str(i) + "_lossed.pgm")
+    # kits.to_img(Y_represented[:, i]).save("C:\\out\\" + str(i) + "_represented.pgm")
 
 print(e)
+